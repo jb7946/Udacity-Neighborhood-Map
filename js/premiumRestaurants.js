b568a41b99e6d734d75a865bfd1e8eead3e6a4a6
jb7946 <jb7946@att.com>
2019-04-24 01:18:31 -0500

    reformatting file to improve readability

--- js/premiumRestaurants.js
+++ js/premiumRestaurants.js
@@ -1,57 +1,56 @@
 // json array containing top five premium restaurants
-var topfive = [
-	{
-		idYelp: 'UO8Hw0dW5yqMN-OPj2UXHg',
-		name: "Costa Brava Bistro",
-		cuisine: "American",
-		coord: {
-								lat: 29.705102,
-								lng: -95.468217
-							}
-	},
-	{
-		idYelp: 'LUhg2OaHsLRskfRpsfUqCg',
-		name: "Blood Bros. BBQ",
-		cuisine: "Barbeque",
-		coord: {
-								lat: 29.705155,
-								lng: -95.475517
-							}
-	},
-	{
-		idYelp: 'tht_b1xZqOjkHZz0TJ-gYw',
-		name: "P. King",
-		cuisine: "Chinese",
-		coord: {
-								lat: 29.70507,
-								lng: -95.47383
-							}
-	},
-	{
-		idYelp: 'fkvfL8n_fPkUuTFl_sP6TA',
-		name: "The '401",
-		cuisine: "American",
-		coord: {
-								lat: 29.70708,
-								lng: -95.47299
-							}
-	},
-	{
-		idYelp: 'hqePmfhjxFpPj_Wyjosm_g',
-		name: "Viet Nom Noms",
-		cuisine: "Vietnamese",
-		coord: {
-								lat: 29.7051133747974,
-								lng: -95.4711166396737
-							}
-	},
-	{
-		idYelp: 'HHMNMlLw0pGrkWFU5zc9SQ',
-		name: "Jax Grill",
-		cuisine: "Burgers",
-		coord: {
-								lat: 29.708158493042,
-								lng: -95.4682006835938
-							}
-	}
+var topfive = [{
+        idYelp: 'UO8Hw0dW5yqMN-OPj2UXHg',
+        name: "Costa Brava Bistro",
+        cuisine: "American",
+        coord: {
+            lat: 29.705102,
+            lng: -95.468217
+        }
+    },
+    {
+        idYelp: 'LUhg2OaHsLRskfRpsfUqCg',
+        name: "Blood Bros. BBQ",
+        cuisine: "Barbeque",
+        coord: {
+            lat: 29.705155,
+            lng: -95.475517
+        }
+    },
+    {
+        idYelp: 'tht_b1xZqOjkHZz0TJ-gYw',
+        name: "P. King",
+        cuisine: "Chinese",
+        coord: {
+            lat: 29.70507,
+            lng: -95.47383
+        }
+    },
+    {
+        idYelp: 'fkvfL8n_fPkUuTFl_sP6TA',
+        name: "The '401",
+        cuisine: "American",
+        coord: {
+            lat: 29.70708,
+            lng: -95.47299
+        }
+    },
+    {
+        idYelp: 'hqePmfhjxFpPj_Wyjosm_g',
+        name: "Viet Nom Noms",
+        cuisine: "Vietnamese",
+        coord: {
+            lat: 29.7051133747974,
+            lng: -95.4711166396737
+        }
+    },
+    {
+        idYelp: 'HHMNMlLw0pGrkWFU5zc9SQ',
+        name: "Jax Grill",
+        cuisine: "Burgers",
+        coord: {
+            lat: 29.708158493042,
+            lng: -95.4682006835938
+        }
+    }
 ];
